f3b603770b2bf99831d982bcd7eaf434eabe24bf
alexsaushkin <thirdyou@yandex.ru>
2023-02-08 18:03:34 +0400

    fix: add lesson4 extension dict

--- lesson4/file_sorter.py
+++ lesson4/file_sorter.py
@@ -2,8 +2,18 @@ import os
 
 
 path_to_download = os.path.join("c:\\", "Users", "user", "Downloads")
+# path_to_download = os.path.join("home", "USERNAME", "Downloads")
+# whoami
 
+extension_parser = {
+    "music": [".mp3", ".m4a"],
+    "video": [".mp4", ".avi"],
+    "image": [".jpg", ".jpeg", ".png", ".gif", ".bmp"],
+    "text": [".txt", ".doc", ".docx", ".pdf", ".xls", ".xlsx", ".ppt", ".pptx"],
+    "archive": [".zip", ".rar", ".gz", ".bz2", ".7z"],
+    "other": [],
+}
 
 
 for i in os.listdir(path_to_download):
-    print(i)
+    print(i.rsplit(".", 1))
